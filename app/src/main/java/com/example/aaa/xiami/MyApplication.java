1e5c5e1137600b24a36e8eff025a526982b1672e
text <908391541@qq.com>
2015-04-28 15:29:12 +0800

    gaidong2

---
+++ app/src/main/java/com/example/aaa/xiami/MyApplication.java
@@ -0,0 +1,14 @@
+package com.example.aaa.xiami;
+
+import android.app.Application;
+
+/**
+ * Created by aaa on 15-4-28.
+ */
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
